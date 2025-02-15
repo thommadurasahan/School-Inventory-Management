500ebb4c8b308336696dc6465d8aca9f13f5d577
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-15 07:33:10 +0530

    Add item properties (inventory)

--- src/main/java/com/begginers/sim/inventory/model/Item.java
+++ src/main/java/com/begginers/sim/inventory/model/Item.java
@@ -1,4 +1,14 @@
 package com.begginers.sim.inventory.model;
 
+import java.util.Date;
+
 public class Item {
+
+    private short itemId;
+    private byte typeId;
+    private String voucherNo;
+    private short quantity;
+    private byte supplierId;
+    private Date insertedDate;
+    private String insertedBy;
 }
