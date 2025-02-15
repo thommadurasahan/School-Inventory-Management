e676619212904171ddf8afafb5dc1af6c77b2de8
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-15 07:54:40 +0530

    Add Getters, Setters to Item (Inventory)
    Use Lombok

--- src/main/java/com/begginers/sim/inventory/model/Item.java
+++ src/main/java/com/begginers/sim/inventory/model/Item.java
@@ -1,7 +1,12 @@
 package com.begginers.sim.inventory.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Item {
 
     private short itemId;
