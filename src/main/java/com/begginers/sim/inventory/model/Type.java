c3b45808727983ebb386b73ff0d1a090359812ac
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-15 15:20:50 +0530

    Add JPA annotations to Type (Inventory)
    Lombok
    Entity
    Id
    GeneratedValue

--- src/main/java/com/begginers/sim/inventory/model/Type.java
+++ src/main/java/com/begginers/sim/inventory/model/Type.java
@@ -1,4 +1,19 @@
 package com.begginers.sim.inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private byte typeId;
+    private String typeName;
 }
