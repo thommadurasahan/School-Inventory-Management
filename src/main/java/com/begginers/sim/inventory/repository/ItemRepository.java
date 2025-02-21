b47c51eb025c9e98f4218fa8bfc6b58c4ef3c7c3
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-21 14:02:20 +0530

    Create ItemRepository (Inventory)

---
+++ src/main/java/com/begginers/sim/inventory/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.begginers.sim.inventory.repository;
+
+import com.begginers.sim.inventory.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Short> {
+}
