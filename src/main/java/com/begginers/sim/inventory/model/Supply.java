11f1f4515d3981016813ba127125196f1c110bb5
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-21 11:28:20 +0530

    Add Type (M) - Supplier (M) relationship

--- src/main/java/com/begginers/sim/inventory/model/Supply.java
+++ src/main/java/com/begginers/sim/inventory/model/Supply.java
@@ -1,9 +1,6 @@
 package com.begginers.sim.inventory.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,4 +14,12 @@ public class Supply {
     private short supplyId;
     private byte typeId;
     private byte supplierId;
+
+    @ManyToOne
+    @JoinColumn(name = "typeId")
+    private Type type;
+
+    @ManyToOne
+    @JoinColumn(name = "supplierId")
+    private Supplier supplier;
 }
