92bacccd40efb0c4b381cec7c22cf34ca8b83bae
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-22 12:28:40 +0530

    Update Item.java (Inventory)

--- src/main/java/com/begginers/sim/inventory/model/Item.java
+++ src/main/java/com/begginers/sim/inventory/model/Item.java
@@ -14,6 +14,7 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private short itemId;
+    private String itemName;
     private String voucherNo;
     private short quantity;
     @Temporal(TemporalType.TIMESTAMP)
