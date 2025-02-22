758494bbba9507ad73fdde6b3a6f34b5f87f6907
Sahan Praneeth <thommadurasahan@outlook.com>
2025-02-22 06:00:35 +0530

    Update ItemController (Inventory)

--- src/main/java/com/begginers/sim/inventory/controller/ItemController.java
+++ src/main/java/com/begginers/sim/inventory/controller/ItemController.java
@@ -2,24 +2,20 @@ package com.begginers.sim.inventory.controller;
 
 import com.begginers.sim.inventory.model.Item;
 import com.begginers.sim.inventory.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/item")
+@RequiredArgsConstructor
 public class ItemController {
 
     private final ItemRepository itemRepository;
 
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
-
+    @PostMapping
     public Item insertItem(@RequestBody Item item) {
         return itemRepository.save(item);
     }
