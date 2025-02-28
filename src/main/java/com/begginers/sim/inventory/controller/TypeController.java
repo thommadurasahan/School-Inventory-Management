881819656603e238c65ae0eac3c08f6435725697
Sahan Praneeth <thommadurasahan@gmail.com>
2025-02-28 20:50:10 +0530

    Add CrossOrigin to inventory classes

--- src/main/java/com/begginers/sim/inventory/controller/TypeController.java
+++ src/main/java/com/begginers/sim/inventory/controller/TypeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/type")
 @RequiredArgsConstructor
 public class TypeController {
