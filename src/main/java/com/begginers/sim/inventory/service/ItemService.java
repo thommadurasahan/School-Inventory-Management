3b4edb9bfc3b652691e88367b9b27d36bdb41d63
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-08 12:50:05 +0530

    Update ItemService

--- src/main/java/com/begginers/sim/inventory/service/ItemService.java
+++ src/main/java/com/begginers/sim/inventory/service/ItemService.java
@@ -2,7 +2,6 @@ package com.begginers.sim.inventory.service;
 
 import com.begginers.sim.inventory.model.Item;
 import com.begginers.sim.inventory.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +10,11 @@ import java.util.Optional;
 @Service
 public class ItemService {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     public List<Item> getAllItems() {
         return itemRepository.findAll();
