b8be3433e5255be5fdb64cb696dbb7a09e3d0518
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-08 10:05:22 +0530

    Create ItemService (Inventory)

--- src/main/java/com/begginers/sim/inventory/service/ItemService.java
+++ src/main/java/com/begginers/sim/inventory/service/ItemService.java
@@ -1,4 +1,32 @@
 package com.begginers.sim.inventory.service;
 
+import com.begginers.sim.inventory.model.Item;
+import com.begginers.sim.inventory.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Optional<Item> getItemById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {
+        itemRepository.deleteById(id);
+    }
 }
