a6e08a75c81d2b588a7afa87e0deaff42ea95008
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-08 13:40:50 +0530

    Crreate SupplierService

---
+++ src/main/java/com/begginers/sim/inventory/service/SupplierService.java
@@ -0,0 +1,34 @@
+package com.begginers.sim.inventory.service;
+
+import com.begginers.sim.inventory.model.Supplier;
+import com.begginers.sim.inventory.repository.SupplierRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SupplierService {
+
+    private final SupplierRepository supplierRepository;
+
+    public SupplierService(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> getSupplierById(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+    public Supplier saveSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public void deleteSupplier(Long id) {
+        supplierRepository.deleteById(id);
+    }
+}
