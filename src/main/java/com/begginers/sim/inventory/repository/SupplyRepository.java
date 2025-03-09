aa8bc72e6a97d198f1b6fd92362427a01b92a15c
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-10 02:19:31 +0530

    Remove relationship table from Inventory

--- src/main/java/com/begginers/sim/inventory/repository/SupplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.begginers.sim.inventory.repository;
-
-import com.begginers.sim.inventory.model.Supply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplyRepository extends JpaRepository<Supply, Long> {
-}
