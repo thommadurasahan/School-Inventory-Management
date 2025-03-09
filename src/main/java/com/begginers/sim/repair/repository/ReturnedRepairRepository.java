a02372007c0aa29a3c348183db7fb4b5bc0db293
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-10 02:33:36 +0530

    Remove relationship tables from Repair

--- src/main/java/com/begginers/sim/repair/repository/ReturnedRepairRepository.java
+++
@@ -1,9 +0,0 @@
-package com.begginers.sim.repair.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.begginers.sim.repair.model.ReturnedRepair;
-
-@Repository
-public interface ReturnedRepairRepository extends JpaRepository<ReturnedRepair, Integer> {
-}
--- src/main/java/com/begginers/sim/repair/repository/ReturnedTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.begginers.sim.repair.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.begginers.sim.repair.model.ReturnedType;
-
-@Repository
-public interface ReturnedTypeRepository extends JpaRepository<ReturnedType, Integer> {
-}
