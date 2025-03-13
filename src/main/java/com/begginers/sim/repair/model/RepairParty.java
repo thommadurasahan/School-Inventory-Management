e0150f65796f2ff071eb22816bbefb68a0f87413
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-13 21:19:12 +0530

    Update RepairParty model
    Co-Authored-By: Charukanc <134363138+Charukanc@users.noreply.github.com>

--- src/main/java/com/begginers/sim/repair/model/RepairParty.java
+++ src/main/java/com/begginers/sim/repair/model/RepairParty.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Setter
 @Getter
 @Entity
@@ -20,28 +18,4 @@ public class RepairParty {
     private String repairPartyName;
     private String contactNo;
     private String address;
-
-    // Constructors
-    public RepairParty() {
-    }
-
-    public static List<RepairParty> getAllRepairParties() {
-
-        throw new UnsupportedOperationException("Unimplemented method 'getAllRepairParties'");
-    }
-
-    public static RepairParty getRepairPartyById(int id) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'getRepairPartyById'");
-    }
-
-    public RepairParty updateRepairParty(int id, RepairParty repairParty) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'updateRepairParty'");
-    }
-
-    public void addRepairParty(int id) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'addRepairParty'");
-    }
 }
