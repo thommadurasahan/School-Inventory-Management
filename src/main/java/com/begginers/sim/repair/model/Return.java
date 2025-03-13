8f84fbdfc9678e9eb94053d8a5ca492948026256
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-13 21:18:34 +0530

    Add Lombok to Return model
    Co-Authored-By: Charukanc <134363138+Charukanc@users.noreply.github.com>

--- src/main/java/com/begginers/sim/repair/model/Return.java
+++ src/main/java/com/begginers/sim/repair/model/Return.java
@@ -1,37 +1,23 @@
 package com.begginers.sim.repair.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@Entity
 public class Return {
     // Todo
     //  Create package exception, include Notfound exception
     //  Create a class ReturnNotFoundException
 
+    @Id
     private int returnId;
     private int repairId;
     private int returnedQuantity;
     private Date returnedOn;
-
-    // Constructors
-    public Return() {}
-
-    public Return(int returnId, int repairId, int returnedQuantity, Date returnedOn) {
-        this.returnId = returnId;
-        this.repairId = repairId;
-        this.returnedQuantity = returnedQuantity;
-        this.returnedOn = returnedOn;
-    }
-
-    // Getters and Setters
-    public int getReturnId() { return returnId; }
-    public void setReturnId(int returnId) { this.returnId = returnId; }
-
-    public int getRepairId() { return repairId; }
-    public void setRepairId(int repairId) { this.repairId = repairId; }
-
-    public int getReturnedQuantity() { return returnedQuantity; }
-    public void setReturnedQuantity(int returnedQuantity) { this.returnedQuantity = returnedQuantity; }
-
-    public Date getReturnedOn() { return returnedOn; }
-    public void setReturnedOn(Date returnedOn) { this.returnedOn = returnedOn; }
 }
