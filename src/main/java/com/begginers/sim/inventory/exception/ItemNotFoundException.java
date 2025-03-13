3d9558a2d4f9922a46dc8580bb6cb7ae07af1648
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-13 11:32:43 +0530

    Create ItemNotFoundException

---
+++ src/main/java/com/begginers/sim/inventory/exception/ItemNotFoundException.java
@@ -0,0 +1,20 @@
+package com.begginers.sim.inventory.exception;
+
+public class ItemNotFoundException extends Exception {
+
+    public ItemNotFoundException() {
+        super();
+    }
+
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+
+    public ItemNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ItemNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
