a7ff4dcb0fbebabdc08f542616fcf40736d56741
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-13 19:37:52 +0530

    Corrected RepairPartyRepository
    Co-Authored-By: Charukanc <134363138+Charukanc@users.noreply.github.com>

--- src/main/java/com/begginers/sim/repair/repository/RepairPartyRepository.java
+++ src/main/java/com/begginers/sim/repair/repository/RepairPartyRepository.java
@@ -1,10 +1,11 @@
 package com.begginers.sim.repair.repository;
 
+import com.begginers.sim.repair.model.RepairParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepairPartyRepository extends JpaRepository<RepairPartyService, Integer> {
+public interface RepairPartyRepository extends JpaRepository<RepairParty, Integer> {
     // ToDo
 //  Crete a custom method, custom query
 }
