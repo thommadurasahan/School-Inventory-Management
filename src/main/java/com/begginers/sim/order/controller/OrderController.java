d35717ebd906cb9dcc0d77ab6538b79ec84a7e09
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-14 10:09:22 +0530

    Resolving duplicate Constant classes

--- src/main/java/com/begginers/sim/order/controller/OrderController.java
+++ src/main/java/com/begginers/sim/order/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(Constant.BASE_API_URL + "/orders")
+@RequestMapping("/orders")
 public class OrderController {
 
     @Autowired
--- src/main/java/com/begginers/sim/util/Constant.java
+++ src/main/java/com/begginers/sim/util/Constant.java
@@ -4,4 +4,8 @@ public class Constant {
 
     // Base URLs
     public static final String API_VERSION = "/api/v1";
+
+    public static final String INVENTORY_BASE_URL = API_VERSION;
+    public static final String ITEM_BASE_URL = INVENTORY_BASE_URL + "/items";
+    
 }
