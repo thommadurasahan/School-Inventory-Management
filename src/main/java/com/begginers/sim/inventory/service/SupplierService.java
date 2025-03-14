824f6980b00327aa67d3c2e112dd259dd21514ad
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-14 14:38:08 +0530

    Make SupplierService an interface and related changes

--- src/main/java/com/begginers/sim/inventory/service/SupplierService.java
+++ src/main/java/com/begginers/sim/inventory/service/SupplierService.java
@@ -2,40 +2,21 @@ package com.begginers.sim.inventory.service;
 
 import com.begginers.sim.inventory.exception.SupplierNotFoundException;
 import com.begginers.sim.inventory.model.Supplier;
-import com.begginers.sim.inventory.repository.SupplierRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-@Slf4j
-public class SupplierService {
+public interface SupplierService {
     // TODO
     //  Change the class to interface, include only method signatures here
     //  Create a new class SupplierServiceImpl, implement it from the Service class and move all code to the Impl class
 
-    private final SupplierRepository supplierRepository;
+    Supplier saveSupplier(Supplier supplier);
 
-    public Supplier saveSupplier(Supplier supplier) {
-        return supplierRepository.save(supplier);
-    }
+    List<Supplier> getAllSuppliers();
 
-    public List<Supplier> getAllSuppliers() {
-        return supplierRepository.findAll();
-    }
+    Supplier getSupplierById(long id) throws SupplierNotFoundException;
 
-    public Supplier getSupplierById(long id) throws SupplierNotFoundException {
-        return supplierRepository.findById(id)
-                .orElseThrow(() -> new SupplierNotFoundException("Supplier not found withID: " + id));
-    }
-
-    public void deleteSupplier(long id) throws SupplierNotFoundException {
-        if (!supplierRepository.existsById(id)) {
-            throw new SupplierNotFoundException("Supplier not found with id: " + id);
-        }
-        supplierRepository.deleteById(id);
-    }
+    void deleteSupplier(long id) throws SupplierNotFoundException;
 }
---
+++ src/main/java/com/begginers/sim/inventory/service/SupplierServiceImpl.java
@@ -0,0 +1,38 @@
+package com.begginers.sim.inventory.service;
+
+import com.begginers.sim.inventory.exception.SupplierNotFoundException;
+import com.begginers.sim.inventory.model.Supplier;
+import com.begginers.sim.inventory.repository.SupplierRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SupplierServiceImpl implements SupplierService {
+
+    private SupplierRepository supplierRepository;
+
+    public Supplier saveSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public Supplier getSupplierById(long id) throws SupplierNotFoundException {
+        return supplierRepository.findById(id)
+                .orElseThrow(() -> new SupplierNotFoundException("Supplier not found withID: " + id));
+    }
+
+    public void deleteSupplier(long id) throws SupplierNotFoundException {
+        if (!supplierRepository.existsById(id)) {
+            throw new SupplierNotFoundException("Supplier not found with id: " + id);
+        }
+        supplierRepository.deleteById(id);
+    }
+}
