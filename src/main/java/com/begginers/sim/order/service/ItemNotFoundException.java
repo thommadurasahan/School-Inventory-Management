9efa11ebb69c956f3d5c9613b34a62c6aef537d1
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-14 10:48:27 +0530

    Delete ItemNotFoundException in Order package

--- src/main/java/com/begginers/sim/order/service/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.begginers.sim.order.service;
-
-public class ItemNotFoundException extends RuntimeException {
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
