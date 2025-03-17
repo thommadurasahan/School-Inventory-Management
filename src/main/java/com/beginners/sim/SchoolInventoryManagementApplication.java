3c7c4d36f22865b5791d28b89c9e2ff8498fcb9d
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-18 05:18:26 +0530

    Add and configure Maven ModelMapper

--- src/main/java/com/beginners/sim/SchoolInventoryManagementApplication.java
+++ src/main/java/com/beginners/sim/SchoolInventoryManagementApplication.java
@@ -2,12 +2,19 @@ package com.beginners.sim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class SchoolInventoryManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolInventoryManagementApplication.class, args);
+    }
+
+    @Bean
+    public ModelMap modelMap() {
+        return new ModelMap();
     }
 
 }
