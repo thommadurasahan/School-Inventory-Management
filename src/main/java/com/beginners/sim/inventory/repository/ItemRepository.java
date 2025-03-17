f75e9cc129e49e9468aa622005445282ba489999
Sahan Praneeth <thommadurasahan@gmail.com>
2025-03-17 12:05:03 +0530

    Add custom query to ItemRepository

--- src/main/java/com/beginners/sim/inventory/repository/ItemRepository.java
+++ src/main/java/com/beginners/sim/inventory/repository/ItemRepository.java
@@ -2,8 +2,14 @@ package com.beginners.sim.inventory.repository;
 
 import com.beginners.sim.inventory.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    // ToDo
-    //  Crete a custom method with native query
+    // Custom query to find items by date
+    @Query(value = "SELECT * FROM items WHERE receivedOn = :receivedOn", nativeQuery = true)
+    List<Item> findItemsByDate(@Param("receivedOn") Date receivedOn);
 }
