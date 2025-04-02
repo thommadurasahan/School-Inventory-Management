4b4226ea76e4518fe75edb299b80d0283882e703
Sahan Praneeth <thommadurasahan@gmail.com>
2025-04-02 10:54:49 +0530

    Update Repari model
    U
    
    Co-Authored-By: Charukanc <134363138+Charukanc@users.noreply.github.com>

--- src/main/java/com/beginners/sim/repair/model/Repair.java
+++ src/main/java/com/beginners/sim/repair/model/Repair.java
@@ -1,20 +1,44 @@
 package com.beginners.sim.repair.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.beginners.sim.inventory.model.Type;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-@Setter
-@Getter
 @Entity
+@Table(name = "repair")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Repair {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "repair_id")
     private int repairId;
+
+    @Column(name = "sent_quantity")
     private int sentQuantity;
-    private Date sendToRepairOn;
+
+    @Column(name = "sent_to_repair_on")
+    private Date sentToRepairOn;
+
+    @Column(name = "repair_location")
     private String repairLocation;
+
+    // Repair (M) - Type (M) Relationship (Owner Side)
+    @ManyToMany
+    @JoinTable(
+            name = "sent_type",
+            joinColumns = @JoinColumn(name = "repair_id"),
+            inverseJoinColumns = @JoinColumn(name = "type_id")
+    )
+    private Set<Type> types = new HashSet<>();
 }
