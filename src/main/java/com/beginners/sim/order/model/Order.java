a57cccb0e64e71ad7007f6c8991304d345127562
Sahan Praneeth <thommadurasahan@gmail.com>
2025-04-02 10:54:24 +0530

    Update Order model
    Co-Authored-By: Thisara Dilakshana Amarasinghe <112304431+rwtdamarasinghe@users.noreply.github.com>

--- src/main/java/com/beginners/sim/order/model/Order.java
+++ src/main/java/com/beginners/sim/order/model/Order.java
@@ -2,27 +2,29 @@ package com.beginners.sim.order.model;
 
 import com.beginners.sim.inventory.model.Supplier;
 import com.beginners.sim.inventory.model.Type;
+import com.beginners.sim.survey.model.Survey;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "order")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
     private Long orderId;
-
-    @Column(name = "item_name")
-    private String itemName;
 
     @Column(name = "quantity")
     private Long quantity;
@@ -41,10 +43,14 @@ public class Order {
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "type_id")
     )
-    private Set<Type> Types = new HashSet<>();
+    private Set<Type> types = new HashSet<>();
 
     // Order (M) - Supplier (1) Relationship (Owner SIde)
     @ManyToOne
     @JoinColumn(name = "supplier_id")
     private Supplier supplier;
+
+    // Survey (1) - Order (1) Relationship (Opposite Side)
+    @OneToOne(mappedBy = "order")
+    private Survey survey;
 }
